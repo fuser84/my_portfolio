4d8a5bee681642c782bea6df33172c977877e2ed
fuser <vnovodid@gmail.com>
2018-04-26 21:00:13 +0300

    add button at the bottom of the page and collapse-menu

--- js/my_portfolio.js
+++ js/my_portfolio.js
@@ -13,11 +13,11 @@ class My_Portfolio_Scroll{
     _scroll(event){
         let top = this._el2.offsetTop;
         event.preventDefault();
-        this._el2.scrollIntoView({behavior: "smooth", block: "start"});
-            // window.scroll({
-            //     top: `${top}`,
-            //     behavior: "smooth",
-            // });
+        //this._el2.scrollIntoView({behavior: "smooth", block: "start"});
+            window.scroll({
+                top: `${top}`,
+                behavior: "smooth",
+            });
     }
 }
 
@@ -29,4 +29,24 @@ let portfolio1 = new My_Portfolio_Scroll({
 let portfolio2 = new My_Portfolio_Scroll({
     el1: document.querySelector('#buttonUp'),
     el2: document.querySelector('#main')
+});
+
+let portfolio3 = new My_Portfolio_Scroll({
+    el1: document.querySelector('#about_me'),
+    el2: document.querySelector('#about')
+});
+
+let portfolio4 = new My_Portfolio_Scroll({
+    el1: document.querySelector('#my_portfolio'),
+    el2: document.querySelector('#portfolio')
+});
+
+let portfolio5 = new My_Portfolio_Scroll({
+    el1: document.querySelector('#my_contact'),
+    el2: document.querySelector('#contact')
+});
+
+let portfolio6 = new My_Portfolio_Scroll({
+    el1: document.querySelector('.navbar-brand'),
+    el2: document.querySelector('#name')
 });
